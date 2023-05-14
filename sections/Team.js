f7120065c5a8cb62e1c2ca615fc6cc891bd94ff5
AbdulrehmanTalat <66178386+AbdulrehmanTalat@users.noreply.github.com>
2023-05-14 20:31:16 +0500

    removed blog page

--- sections/Team.js
+++ sections/Team.js
@@ -1,7 +1,6 @@
 import { teamdata } from "@/assets/data/dummydata"
 import { Card } from "@/components/common/Card"
 import { Title, TitleSm } from "@/components/common/Title"
-import team from "@/pages/team"
 import React from "react"
 
 const Team = () => {
